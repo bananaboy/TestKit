5e01c5d69a2a93c934870a5a9c73147d66b71df8
Sam Izzo <sam@humbug.net>
2014-03-26 00:23:58 +1100

    * Added reset stylesheet, added open sans font reference

--- application/views/header.php
+++ application/views/header.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="utf-8" />
     
-    <title>Welcome to PIP</title>
+    <title>TestKit</title>
     <meta name="description" content="">
     <meta name="author" content="">
-    
+
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>static/css/reset.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>static/css/style.css" type="text/css" media="screen" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 </head>
 <body>

9c5526fdf648adb65d180a6e9954872599e709df
Sam Izzo <sam@humbug.net>
2014-04-23 14:28:01 +1000

    * Refactored table to use bootstrap tables * Added action button to send invite or remove testers (send invite currently does nothing) * 'Select all' checkbox now works

--- application/controllers/testers.php
+++ application/controllers/testers.php
@@ -11,45 +11,58 @@ class Testers extends Controller
 
     function index()
     {
-        $testers = $this->model->findAll();
-        $this->render(array("testers" => $testers));
-    }
-
-    function addremove()
-    {
         $ph = $this->loadHelper('Post_helper');
 
-        if ($ph->hasValue('add') && !$ph->hasValue('remove'))
+        if ($ph->hasValue('tester-add'))
         {
             // Add a new tester.
             $this->add($ph);
         }
-        else if ($ph->hasValue('remove') && !$ph->hasValue('add'))
+        else if ($ph->hasValue('tester-action') && strlen($ph->get('tester-action')) > 0)
         {
-            // Remove testers.
-            $this->remove($ph);
+            $action = $ph->get('tester-action');
+            switch ($action)
+            {
+                case 'tester-action-send-invites':
+                {
+                    $this->sendInvites($ph);
+                    break;
+                }
+
+                case 'tester-action-remove-testers':
+                {
+                    $this->remove($ph);
+                    break;
+                }
+            }
         }
 
-        $this->redirect('testers');
+        $testers = $this->model->findAll();
+        $this->render(array("testers" => $testers));
     }
 
     private function add($ph)
     {
-        if ($ph->hasValue('email') && strlen($ph->get('email')) > 0)
+        if ($ph->hasValue('tester-email') && strlen($ph->get('tester-email')) > 0 &&
+            $ph->hasValue('tester-first-name') && strlen($ph->get('tester-first-name')) > 0 &&
+            $ph->hasValue('tester-last-name') && strlen($ph->get('tester-last-name')) > 0)
         {
-            $email = $ph->get('email');
+            $email = $ph->get('tester-email');
+            $firstName = $ph->get('tester-first-name');
+            $lastName = $ph->get('tester-last-name');
             if (filter_var($email, FILTER_VALIDATE_EMAIL))
             {
-                $this->model->insert(array("email" => $email));
+                $this->model->insert(array("email" => $email, "first_name" => $firstName, "last_name" => $lastName));
             }
         }
     }
 
     private function remove($ph)
     {
-        if ($ph->hasValue('removeTester'))
+        if ($ph->hasValue('tester-select'))
         {
-            $toRemove = $ph->get('removeTester');
+            $toRemove = $ph->get('tester-select');
+            error_log(print_r($toRemove, true));
             $toRemove = array_keys($toRemove);
             $this->model->delete('id', $toRemove);
         }
--- application/views/testers.php
+++ application/views/testers.php
@@ -4,43 +4,86 @@
 <?php include('navbar.php'); ?>
 
     <div class="main">
-        <form method="post" name="addtester" id="addtester" action="<?php echo BASE_URL; ?>testers/addremove">
-            <table>
-                <tr>
-                    <th>Email</th>
-                    <th>&nbsp;</th>
-                </tr>
-                <?php
-                    // Output all testers into the table.
-                    function printTester($tester, $key)
-                    {
-                        echo "<tr>";
-                        echo "<td>$tester->email</td>";
-                        echo "<td><input type='checkbox' name='removeTester[$tester->id]'></input></td>";
-                        echo "</tr>";
-                    }
-                    array_walk($testers, 'printTester');
-                ?>
-                <tr>
-                    <td>&nbsp;</td>
-                    <td>
-                        <input type="submit" name="remove" id="remove" value="Remove" class="button">
-                    </td>
-                </tr>
-                <tr><td colspan="2">&nbsp;</td></tr>
-                <tr><td colspan="2">&nbsp;</td></tr>
-                <tr><td colspan="2">&nbsp;</td></tr>
-                <tr>
-                    <td>
-                        <label>Email address
-                            <input type="text" class="text" id="email" name="email">
-                        </label>
-                        <input type="submit" name="add" id="add" value="Add" class="button">
-                    </td>
-                    <td>&nbsp;</td>
-                </tr>
+        <form method="post" name="tester-form" id="tester-form" action="<?php echo BASE_URL; ?>testers">
+            <input type="hidden" name="tester-action" id="tester-action" value="">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th class="table-checkbox"><input type="checkbox" id="select-all" class="table-checkbox"></th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <td colspan="3">&nbsp;</td>
+                        <td class="table-checkbox">
+                            <div class="btn-group btn-action">
+                                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                                    Action <span class="caret"></span>
+                                </button>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#" class="send-invite tester-action-menu" id="tester-action-send-invites">Send invites</a></li>
+                                    <li><a href="#" class="remove-tester tester-action-menu" id="tester-action-remove-testers">Remove testers</a></li>
+                                </ul>
+                            </div>
+                        </td>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    <?php
+                        // Output all testers into the table.
+                        function printTester($tester, $key)
+                        {
+                            echo "<tr>";
+                            echo "<td>$tester->first_name</td>";
+                            echo "<td>$tester->last_name</td>";
+                            echo "<td>$tester->email</td>";
+                            echo "<td class='table-checkbox'><input type='checkbox' class='table-checkbox tester-checkbox' name='tester-select[$tester->id]'></td>";
+                            echo "</tr>";
+                        }
+                        array_walk($testers, 'printTester');
+                    ?>
+                </tbody>
             </table>
+
+            <br/>
+            <br/>
+            <hr />
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3"><input type="text" class="form-control" id="first-name" name="tester-first-name" placeholder="first name"></div>
+                    <div class="col-md-3"><input type="text" class="form-control" id="last-name" name="tester-last-name" placeholder="last name"></div>
+                    <div class="col-md-3"><input type="text" class="form-control" id="email" name="tester-email" placeholder="email address"></div>
+                    <div class="col-md-3"><input type="submit" name="tester-add" id="add" value="Add" class="btn btn-default tkbutton add-tester"></div>
+                </div>
+            </div>
         </form>
     </div>
+
+<script>
+    /*
+        Select or unselect all testers when the 'select all' box is ticked/unticked.
+    */
+    $('#select-all').change(
+        function()
+        {
+            $('.tester-checkbox').prop('checked', $(this).prop('checked'));
+        }
+    );
+
+    /*
+        Submit the form when add/send invite is selected.
+    */
+    $('.tester-action-menu').click(
+        function()
+        {
+            $('#tester-action').val($(this).attr('id'));
+            $('#tester-form').submit();
+        }
+    );
+</script>
 
 <?php include('footer.php'); ?>
